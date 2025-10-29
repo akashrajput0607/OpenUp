60b96c285562cbcd2799abe0d998af57bda4d407
theg0607 <akashrajput2363@gmail.com>
2025-10-29 15:29:08 +0530

    first commit

---
+++ src/app/components/Auth/Signin.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Signin() {
+  return (
+    <div>
+        Signin
+      <button>signup</button>
+
+    </div>
+  )
+}
+
+export default Signin
---
+++ src/app/components/Auth/Signup.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Signup() {
+  return (
+    <div>
+      signup
+      <button>Login</button>
+    </div>
+  )
+}
+
+export default Signup
